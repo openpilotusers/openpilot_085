d111f8afd626b596c88423f778e8419f5c6660f3
Vehicle Researcher <>
2021-06-14 15:03:42 +0900

    openpilot v0.8.5

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -63,7 +63,7 @@ class Controls:
 
     self.sm = sm
     if self.sm is None:
-      ignore = ['driverCameraState', 'managerState'] if SIMULATION else ['driverCameraState', 'driverMonitoringState', 'driverState']
+      ignore = ['driverCameraState', 'managerState'] if SIMULATION else ['driverCameraState', 'driverMonitoringState', 'driverState', 'radarState', 'longitudinalPlan', 'modelV2']
       self.sm = messaging.SubMaster(['deviceState', 'pandaState', 'modelV2', 'liveCalibration',
                                      'driverMonitoringState', 'longitudinalPlan', 'lateralPlan', 'liveLocationKalman',
                                      'managerState', 'liveParameters', 'radarState'] + self.camera_packets,
@@ -185,7 +185,6 @@ class Controls:
     self.steerRatio_to_send = 0
     
     self.model_long_alert_prev = True
-    self.delayed_comm_issue_timer = 0
 
   def update_events(self, CS):
     """Compute carEvents from carState"""
@@ -263,17 +262,12 @@ class Controls:
     elif not self.sm.valid["pandaState"]:
       self.events.add(EventName.usbError)
     elif not self.sm.all_alive_and_valid() and self.sm['pandaState'].pandaType != PandaType.whitePanda and not self.commIssue_ignored:
-      self.delayed_comm_issue_timer += 1
-      if self.delayed_comm_issue_timer > 100 and not Params().get_bool("OpkrMapEnable"):
-        self.events.add(EventName.commIssue)
-      elif self.delayed_comm_issue_timer > 200 and Params().get_bool("OpkrMapEnable"):
-        self.events.add(EventName.commIssue)
+      self.events.add(EventName.commIssue)
       if not self.logged_comm_issue:
         cloudlog.error(f"commIssue - valid: {self.sm.valid} - alive: {self.sm.alive}")
         self.logged_comm_issue = True
     else:
       self.logged_comm_issue = False
-      self.delayed_comm_issue_timer = 0
 
     if not self.sm['lateralPlan'].mpcSolutionValid and not (EventName.laneChangeManual in self.events.names) and CS.steeringAngleDeg < 15:
       self.events.add(EventName.plannerError)
@@ -316,7 +310,7 @@ class Controls:
       #    (not TICI or self.enable_lte_onroad):
       #    # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
       #    self.events.add(EventName.noGps)
-      if not self.sm.all_alive(self.camera_packets):
+      if not self.sm.all_alive(self.camera_packets) and CS.vEgo > 0.3:
         self.events.add(EventName.cameraMalfunction)
       if self.sm['modelV2'].frameDropPerc > 20:
         self.events.add(EventName.modeldLagging)
@@ -525,9 +519,8 @@ class Controls:
     a_acc_sol = long_plan.aStart + (dt / LON_MPC_STEP) * (long_plan.aTarget - long_plan.aStart)
     v_acc_sol = long_plan.vStart + dt * (a_acc_sol + long_plan.aStart) / 2.0
 
-    extras_loc = {'lead_one': self.sm['radarState'].leadOne, 'has_lead': long_plan.hasLead}
     # Gas/Brake PID loop
-    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, long_plan.vTargetFuture, long_plan.aTarget, a_acc_sol, self.CP, long_plan.hasLead, self.sm['radarState'], long_plan.longitudinalPlanSource, extras_loc)
+    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, long_plan.vTargetFuture, long_plan.aTarget, a_acc_sol, self.CP, long_plan.hasLead, self.sm['radarState'], long_plan.longitudinalPlanSource)
 
     # Steering PID loop and lateral MPC
     actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(self.active, CS, self.CP, self.VM, params, lat_plan)
