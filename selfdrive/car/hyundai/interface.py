0c8aa363b79158e47187471df38baac826a35dd7
openpilotusers <>
2021-06-17 23:42:46 +0900

    openpilot v0.8.5

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -217,14 +217,14 @@ class CarInterface(CarInterfaceBase):
     ret.brakeMaxV = [0.7, 3.0]   # max brake allowed
 
     ret.longitudinalTuning.kpBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kpV = [0.5, 0.75, 0.65, 0.38, 0.28, 0.19]
+    ret.longitudinalTuning.kpV = [0.45, 0.75, 0.65, 0.38, 0.28, 0.19]
     ret.longitudinalTuning.kiBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kiV = [0.015, 0.026, 0.028, 0.019, 0.018, 0.016]
+    ret.longitudinalTuning.kiV = [0.014, 0.025, 0.027, 0.019, 0.018, 0.016]
 
     ret.longitudinalTuning.deadzoneBP = [0., 9., 17.]
     ret.longitudinalTuning.deadzoneV = [0., 0.1, 0.05]
     ret.longitudinalTuning.kdBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kdV = [1.0, 1.1, 0.8, 0.75, 0.55, 0.45]
+    ret.longitudinalTuning.kdV = [1.0, 0.9, 0.8, 0.75, 0.55, 0.45]
     ret.longitudinalTuning.kfBP = [0., 4., 9., 17., 23., 31.]
     ret.longitudinalTuning.kfV = [1.0, 1.1, 1.5, 1.0, 0.9, 0.8]
 
