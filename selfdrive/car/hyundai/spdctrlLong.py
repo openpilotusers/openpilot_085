f84e53c3ae455a0504f1481ded3453fd8ccddc58
Vehicle Researcher <>
2021-06-18 11:08:49 +0900

    openpilot v0.8.5

--- selfdrive/car/hyundai/spdctrlLong.py
+++ selfdrive/car/hyundai/spdctrlLong.py
@@ -107,12 +107,15 @@ class SpdctrlLong(SpdController):
             elif CS.clu_Vanz > 60 and lead_objspd < -2 and 30 < int(CS.clu_Vanz) <= CC.setspeed and int(CS.clu_Vanz) >= dRel*1.9 and 1 < dRel < 149: # 유지거리 범위 외 감속 조건 앞차 감속중 현재속도/2 아래로 거리 좁혀졌을 때 상대속도에 따라 점진적 감소
                 self.seq_step_debug = "SS>VS,v>60,-1"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed(CS, CC, max(25, 60+(lead_objspd*2)), -1)
-            elif CS.clu_Vanz > 40 and lead_objspd < -2 and 30 < int(CS.clu_Vanz) <= CC.setspeed and int(CS.clu_Vanz) >= dRel*2.2 and 1 < dRel < 149: # 유지거리 범위 외 감속 조건 앞차 감속중 현재속도/2 아래로 거리 좁혀졌을 때 상대속도에 따라 점진적 감소
-                self.seq_step_debug = "SS>VS,v>40,-1"
-                lead_wait_cmd, lead_set_speed = self.get_tm_speed(CS, CC, max(25, 70+(lead_objspd*2)), -1)
-            elif 60 > CS.clu_Vanz > 30 and lead_objspd < -3 and 30 < int(CS.clu_Vanz) <= CC.setspeed and int(CS.clu_Vanz) >= dRel*0.85 and 1 < dRel < 149:
+            # elif CS.clu_Vanz > 40 and lead_objspd < -2 and 30 < int(CS.clu_Vanz) <= CC.setspeed and int(CS.clu_Vanz) >= dRel*2.2 and 1 < dRel < 149: # 유지거리 범위 외 감속 조건 앞차 감속중 현재속도/2 아래로 거리 좁혀졌을 때 상대속도에 따라 점진적 감소
+            #     self.seq_step_debug = "SS>VS,v>40,-1"
+            #     lead_wait_cmd, lead_set_speed = self.get_tm_speed(CS, CC, max(25, 70+(lead_objspd*2)), -1)
+            elif 65 > CS.clu_Vanz > 30 and lead_objspd < -3 and 30 < int(CS.clu_Vanz) <= CC.setspeed and int(CS.clu_Vanz) >= dRel*0.85 and 1 < dRel < 149:
                 self.seq_step_debug = "SS>VS,60>v>30,-1"
-                lead_wait_cmd, lead_set_speed = self.get_tm_speed(CS, CC, max(25, 250-(abs(lead_objspd**3))), -1)
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed(CS, CC, max(25, 230-(abs(lead_objspd**3))), -1)
+            elif 65 > CS.clu_Vanz > 30 and lead_objspd < 0 and 30 < int(CS.clu_Vanz)+4 < CC.setspeed and int(CS.clu_Vanz) >= dRel*0.85 and 1 < dRel < 149:
+                self.seq_step_debug = "감속준비"
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, CC, 25, -1)
             elif 7 < int(CS.clu_Vanz) < 30 and lead_objspd < 0 and CC.setspeed > 30:
                 self.seq_step_debug = "SS>VS,30이하"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed(CS, CC, 25, -5)
